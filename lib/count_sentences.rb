e8e070e77d9aa7956cf944484e60f41e2edeff27
AyubuAyubu <ayubu6143@gmail.com>
2022-10-23 08:22:45 +0300

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?('.')
   end
 
   def question?
-
+      self.end_with?('?')
   end
 
   def exclamation?
-
+       self.end_with?('!')
   end
 
+  # returns the number of sentences in a complex string [split(/[.?!]/)]
   def count_sentences
-
+    #counts_end =
+    array_sentence = self.split(/[.?!] /)
+    array_sentence.length
   end
 end
